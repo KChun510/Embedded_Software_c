ea9a46cfdf914915706a9a09ee3802b373d9464c
Kchun510 <kylechun1@gmail.com>
2025-04-11 13:24:12 -0700

    Adding diagram dir, for circ diagrams of each proj

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 /*
  * embeddedSysProject.c
- *
  * Created: 4/9/2025 7:20:24 PM
  * Author : westcoast
  */
@@ -19,21 +18,16 @@
 	}
 }
 
-int main(void)
-{
-    /* Replace with your application code */
-    // This is how you set the read and write of B series ports
-    // If you wanted to get access to the A series of ports you would use DDRA 
-	// Use PORT for writing
+int main(void){
+	// If you wanted to get access to the A series of ports you would use DDRA 
+	// // Use PORT for writing
 	// USE PIN for reading
-    DDRB = DDRB | 0x01;
+	DDRB = DDRB | 0x01;
 	avr_wait(3000);
-    while (1) {
-		
+	    while (1) {
 		if(!(PINB & 0x02)){
 			check_button_press();
 		}
-		
 		if(valid_press){
 			for(unsigned char i = 0; i < 0x03; ++i){
 				PORTB = 0x01;
@@ -45,7 +39,6 @@ int main(void)
 		}else{
 			PORTB = 0x00;
 		}
-		
     }
 }
 
